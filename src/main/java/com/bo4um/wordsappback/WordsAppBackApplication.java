e74d24c70416b8dbb0e08775304aa25d0db7086b
dmitrybelyatskiy <dmitry.belyatskiy@gmail.com>
2026-03-07 12:12:19 +0300

    initial commit

---
+++ src/main/java/com/bo4um/wordsappback/WordsAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.bo4um.wordsappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsAppBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordsAppBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bo4um/wordsappback/WordsAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bo4um.wordsappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
